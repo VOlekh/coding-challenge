7bd068f99f87e263cfdef7d3be1b950026a7e729
Valentina-Olekhnovich <volekh@gmail.com>
2022-01-25 17:01:30 +0100

    Add information tooltip on right button

--- apps/webextension/src/app/v-sidebar-component/v-sidebar-component.tsx
+++ apps/webextension/src/app/v-sidebar-component/v-sidebar-component.tsx
@@ -11,11 +11,12 @@ export function VSidebarComponent(props: VSidebarComponentProps) {
       <div className="flex items-center min-h-screen bg-gray-50 dark:bg-gray-900">
         <div className="container mx-auto">
           <div className="max-w-md mx-auto my-10 bg-white p-5 rounded-md shadow-sm">
+            {/* close button */}
             <button
               type="button"
-              className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+              className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-4 focus:ring-blue-300"
             >
-              <span className="sr-only">Close menu</span>
+              <span className="sr-only">Close</span>
               {/* <!-- Heroicon name: outline/x --> */}
               <svg
                 className="h-6 w-6"
@@ -53,10 +54,37 @@ export function VSidebarComponent(props: VSidebarComponentProps) {
                 </div>
 
                 <div className="mb-6">
-                  <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
-                    Event types
-                  </label>
+                  <div className="flex items-center">
+                    <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
+                      Event types
+                    </label>
 
+                    {/* Show tooltip on right */}
+                    <button
+                      data-tooltip-target="tooltip-right"
+                      data-tooltip-placement="right"
+                      type="button"
+                      className="mb-2 ml-3 md:mb-0 text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-0,5 text-center"
+                    >
+                      i
+                    </button>
+                    <div
+                      id="tooltip-right"
+                      role="tooltip"
+                      className="tooltip absolute z-10 inline-block bg-gray-900 font-medium shadow-sm text-white py-2 px-3 text-sm rounded-lg opacity-0 invisible"
+                      data-popper-reference-hidden=""
+                      data-popper-escaped=""
+                      data-popper-placement="right"
+                      // style="position: absolute; inset: 0px auto auto 0px; margin: 0px; transform: translate3d(1162px, 1620px, 0px);"
+                    >
+                      Information about event types on right
+                      <div
+                        className="tooltip-arrow"
+                        data-popper-arrow=""
+                        // style="position: absolute; top: 0px; transform: translate3d(0px, 28px, 0px);"
+                      ></div>
+                    </div>
+                  </div>
                   <select
                     className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
                     name="eventType"
@@ -73,8 +101,9 @@ export function VSidebarComponent(props: VSidebarComponentProps) {
                     Save placeholders
                   </label>
 
+                  {/* checkbox */}
                   <input
-                    className="m-6 "
+                    className="m-6 focus:ring-4 focus:ring-blue-300 "
                     type="checkbox"
                     id="scales"
                     name="scales"
