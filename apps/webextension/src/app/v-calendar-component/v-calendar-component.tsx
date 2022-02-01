a829aafbf2d052eea9664a54c70015e954ee7733
Valentina-Olekhnovich <volekh@gmail.com>
2022-02-01 21:27:54 +0100

    inventing function to show  text in text box

--- apps/webextension/src/app/v-calendar-component/v-calendar-component.tsx
+++ apps/webextension/src/app/v-calendar-component/v-calendar-component.tsx
@@ -10,38 +10,52 @@ export interface VCalendarComponentProps {
   availabilities?: Array<string>;
 }
 
-// interface Set<availabilityValue> {
-//   add(value: availabilityValue): Set<availabilityValue>;
-//   has(value: availabilityValue): boolean;
-//   delete(value: availabilityValue): boolean;
-// }
+
 
 export function VCalendarComponent(props: VCalendarComponentProps) {
   //duration will come from form component TBD
   const duration = 15;
-  // const [availabilitiesValues, setAvailabilities] = useState(new Set());
   const [selectedIntervals, setSelectedIntervals] = useState([]);
   const selectedAvailableIntervals = JSON.stringify(selectedIntervals);
+  console.log('selectedAvailableIntervals', selectedAvailableIntervals);
+
+  
+  //console.log('selecteIntervals', selectedIntervals.map(i=>i.value));
 
   const [selectedDates, setSelectedDates] = useState<
     DateObject | DateObject[]
   >();
   const selectedAvailableDates = JSON.stringify(selectedDates);
-  const textToCopy = `Following dates ${selectedAvailableDates} and time slots ${selectedAvailableIntervals}`
 
-  // const handleSelectAvailabilities = (
-  //   e: React.ChangeEvent<HTMLSelectElement>
-  // ) => {
-  //   const selectedValue = e.target.value;
-  //   console.log(selectedValue);
-  //   if (!availabilitiesValues.has(selectedValue)) {
-  //     availabilitiesValues.add(selectedValue);
-  //   } else {
-  //     availabilitiesValues.delete(selectedValue);
-  //   }
-  //   console.log(availabilitiesValues);
-  //   setAvailabilities(availabilitiesValues);
-  // };
+
+  // const selectedAvailableDates = "1645036878403,1645209678403,1645641678403";
+  function test(selectedAvailableDates?:any){
+  
+  const result ="";
+  if (
+    selectedAvailableDates !== null &&
+    selectedAvailableDates !== undefined &&
+    selectedAvailableDates.toString().trim().length !== 0
+  ) 
+  {
+    const result = selectedAvailableDates.split(",").map((date:any) => new Date(parseInt(date)).toLocaleDateString("UTC"))
+    
+  console.log(result)
+  
+  } else {
+  
+   console.log("Error")
+  }
+  return result
+  }
+  
+  test(selectedAvailableDates)
+
+
+
+
+  const textToCopy = `Following dates ${ test(selectedAvailableDates)} and time slots ${selectedAvailableIntervals}`;
+
 
   // generate array of objects for drop-down values based on duration
   function getTimeStops(start: any, end: any) {
@@ -78,24 +92,6 @@ export function VCalendarComponent(props: VCalendarComponentProps) {
   return (
     <div>
       <h1>Welcome to VCalendarComponent!</h1>
-      {/* 
-      <div className="mb-6">
-        <label className="block mb-2 text-sm text-gray-600 dark:text-gray-400">
-          Availabilities
-        </label>
-
-        <select
-          className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
-          name="availabilities"
-          id="availabilities"
-          multiple={true}
-          onChange={(e) => handleSelectAvailabilities(e)}
-        >
-          {formattedTimeStops.map((i) => {
-            return <option value={i.value}>{i.label}</option>;
-          })}
-        </select>
-      </div> */}
 
       <div>
         <MultiSelect
@@ -128,16 +124,12 @@ export function VCalendarComponent(props: VCalendarComponentProps) {
           placeholder="Start selecting availibilities you would like to share on the calendar"
           className="w-200 px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-indigo-100 focus:border-indigo-300 dark:bg-gray-700 dark:text-white dark:placeholder-gray-500 dark:border-gray-600 dark:focus:ring-gray-900 dark:focus:border-gray-500"
           required
-          //onChange={setAvailibilities}
-          //value={form?.availabilities}
           value={textToCopy}
         ></textarea>
       </div>
       <button
         className="w-full px-3 py-4 text-white bg-blue-500 rounded-md focus:bg-indigo-600 focus:outline-none"
-        onClick={() =>
-          navigator.clipboard.writeText(textToCopy)
-        }
+        onClick={() => navigator.clipboard.writeText(textToCopy)}
       >
         Copy to Clipboard
       </button>
